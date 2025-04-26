52a2433da8689da63f8180dbfce2b846f2bce167
khouloudmanai <167105732+khouloudmanai@users.noreply.github.com>
2025-04-26 17:21:27 +0200

    Delete Desktop/MyProjectJs directory

--- Desktop/MyProjectJs/firstProject/first-app-mongodb/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoos = require('mongoose')
-const userSchema = new mongoos.Schema({
-    name:String,
-    email:{required:true,unique:true,type:String},
-    age:Number
-})
-
-module.exports = mongoos.model('user', userSchema)
--- Desktop/MyProjectJs/firstSeance.js
+++
@@ -1,6 +0,0 @@
-const firstname = "saif";
-const lastname = "ridene";
-const age = 25;
-
-const user = { firstname, lastname, age }; // shorthand property syntax
-console.log(user);
